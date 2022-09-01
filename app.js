01f070708ad5fb6c84e6bc9fdb2298dafff8785a
Coleman <charleebrown22@gmail.com>
2022-09-01 13:08:23 -0500

    understanding CORS

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin:  https://mainsendapi.herokuapp.com");
+  res.header("Access-Control-Allow-Origin:  CharleeBrown.github.io/EntryApp");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
