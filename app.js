221dbf7c9e713a331a163c6034e522d0af28d26b
Coleman <charleebrown22@gmail.com>
2022-09-01 13:10:30 -0500

    Adding more CORS

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin:  https://mainsendapi.herokuapp.com");
-  res.header("Access-Control-Allow-Origin:  CharleeBrown.github.io/EntryApp");
+  res.header("Access-Control-Allow-Origin:  http://localhost:3001");
+    res.header("Access-Control-Allow-Origin:  http://localhost:3000");
+  res.header("Access-Control-Allow-Origin:  CharleeBrown.github.io");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
