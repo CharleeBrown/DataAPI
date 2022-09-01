5f858be9e4dcd22f169488e9582e0a6426446b23
Coleman <charleebrown22@gmail.com>
2022-09-01 11:31:55 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ router.get('/', function(req, res,next){
 })
 /* GET home page. */
 router.post('/', function(req, res, next) {
-
+  let newObj = {"name":req.body['name']}
+  console.log(newObj);
     client.connect((err) => 
     {
-      let newObj = {"name":req.body['name']}
       collect.insertOne(newObj);
     });    
 
