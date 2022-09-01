4246ee3c3c248ec9584821a4732358a80512dc60
Coleman <charleebrown22@gmail.com>
2022-09-01 09:17:20 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+const {MongoClient} = require("mongodb");
+const uri = config.connString;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const collect = client.db("mainDB").collection("CalLabBooking");
+
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+
+    client.connect(async (err) => 
+    {
+      let newObj = {"name":req.body.name}
+      collect.insertOne(newObj);
+    });    
+});
+
+
+
+module.exports = router;
