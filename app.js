4b7b76b3acf4c99ce002da0acc0514ca13ea6805
Coleman <charleebrown22@gmail.com>
2022-09-01 13:30:38 -0500

    added cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,7 +25,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
-})
+});
+app.use(cors());
 app.use('/', indexRouter);
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
